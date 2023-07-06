5db79c53377baec80d2506e40687c836af34c511
rrtechmart <reza.cpbd@gmail.com>
2023-07-06 06:43:51 +0600

    adjusting some responsive as we needed

--- src/components/Home/Reservation.jsx
+++ src/components/Home/Reservation.jsx
@@ -7,13 +7,13 @@ const Reservation = () => {
     return (
         <div>
             <h1 className='text-6xl font-bold'>Make a Reservation </h1>
-            <div className='grid grid-cols-2 my-10'>
-                <div className=''>
+            <div className='grid md:grid-cols-2 my-10'>
+                <div className='mb-2'>
                     <img className='rounded-lg' src={reservation} alt="" />
                 </div>
                 <div className='w-2/3 mx-auto'>
                     <img className='rounded-lg h-[350px] mb-10' src={booking} alt="" />
-                    <Link className="btn btn-primary ml-6 w-[350px] text-2xl font-bold"> <button >Make Reserve</button> </Link>
+                    <Link className="btn btn-primary ml-1 md:ml-6 md:w-[350px] text-2xl font-bold"> <button >Make Reserve</button> </Link>
                 </div>
             </div>
             
